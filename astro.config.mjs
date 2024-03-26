830390bda2f55fc4d8315d2d156e836e9181d01e
Anders Åberg <anders@andersaberg.com>
2024-03-26 18:51:07 +0100

    Added mock content

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Bitwarden Developers',
+			social: {
+				github: 'https://github.com/bitwarden',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
